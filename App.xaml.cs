093d5af189fc33f452fadf2acbb0af52b219bc74
Michu <michelle.reyes.delgado@udla.edu.ec>
2025-06-03 07:20:20 -0500

    First Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReyesMProgII;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReyesMProgII;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
