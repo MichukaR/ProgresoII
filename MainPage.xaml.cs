091dee7a9c61a4eba8d04ea78cd6337e9d2ee8fe
Michu <michelle.reyes.delgado@udla.edu.ec>
2025-06-03 07:56:15 -0500

    Main Page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,22 +2,17 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
     }
-
-    private void OnCounterClicked(object sender, EventArgs e)
+    
+    private void BotonAChistes(object? sender, EventArgs e)
     {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        Navigation.PushAsync(new ChistesView());
+    }
+    private void BotonAAbout(object? sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AboutView());
     }
 }
